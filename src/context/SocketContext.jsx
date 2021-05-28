e751b29a17de164b98b77f10c318a6e0c8de51d0
Oriol Mauri <oriol@bmauri.com>
2021-05-28 16:50:41 +0200

    Display Context on Raspberry Screen

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -3,9 +3,7 @@ import socketIOClient from 'socket.io-client';
 
 const SocketContext = createContext({ socket: null });
 
-// const IP = process.env.SOCKET_IP || window.location.host
-// const PORT = process.env.SOCKET_PORT || window.location.port
-const socketUrl = 'http://172.25.229.137:3001';
+const socketUrl = process.env.SOCKET_URL || 'http://localhost:3001';
 
 console.log(`Socket: ${socketUrl}`);
 
